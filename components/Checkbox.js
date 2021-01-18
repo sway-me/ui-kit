114d0a5123827e77bf22df25700fdbb102e137c0
Resister <iot.resister@protonmail.com>
2021-01-18 11:26:00 -0600

    Initial commit

---
+++ components/Checkbox.js
@@ -0,0 +1,58 @@
+class Checkbox extends HTMLElement {
+    constructor() {
+        // Always call super first in constructor
+        super();
+
+        // Create a shadow root
+        const shadow = this.attachShadow({mode: 'open'});
+
+        const checkbox = document.createElement('div')
+        checkbox.setAttribute('class', 'checkbox')
+
+
+
+
+        // Create some CSS to apply to the shadow dom
+        const style = document.createElement('style');
+
+        style.textContent = `
+            .checkbox {
+                background: var(--pink-gradient);
+                font-size: 1rem;
+                color: white;
+                cursor: pointer;
+                width: 20px;
+                height: 20px;
+                display: flex;
+                align-items: center;
+                justify-content: center; 
+                border-radius: 5px;
+                border: var(--border);
+                box-shadow: var(--light-shadow);
+                  -moz-user-select: none; 
+                  -webkit-user-select: none; 
+            }
+
+    `;
+
+        // Attach the created elements to the shadow dom
+        shadow.appendChild(style);
+        shadow.appendChild(checkbox);
+
+
+        checkbox.onclick = () => {
+               if(!checkbox.textContent) {
+                   checkbox.textContent = "✓"
+                   this.setAttribute('checked', 'checked')
+               } else {
+                   checkbox.textContent = ""
+                   this.removeAttribute('checked')
+               }
+
+
+
+        }
+    }
+}
+
+customElements.define('check-box', Checkbox);
