860f3115cc0923c42db8aa7062d73f9b6f0afd87
resister <iot.resister@protonmail.com>
2021-02-23 20:25:40 -0600

    Remove build file

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./components/index.js",
   output: {
-    filename: "sway-components.js",
+    filename: "sway-ui.js",
     path: path.resolve(__dirname, "."),
   },
 };
