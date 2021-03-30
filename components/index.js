b359e86c61040d9415016c6276af139e050af238
resister <iot.resister@protonmail.com>
2021-03-29 19:58:30 -0500

    removed checkbox

--- components/index.js
+++ components/index.js
@@ -2,6 +2,6 @@ import Checkbox from "./sway-checkbox";
 import Footer from "./sway-footer";
 import Nav from "./sway-nav";
 
-customElements.define("sway-checkbox", Checkbox);
+//customElements.define("sway-checkbox", Checkbox);
 customElements.define("sway-footer", Footer);
 customElements.define("sway-nav", Nav);
